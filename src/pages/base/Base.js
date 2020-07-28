c1211ac24a130cdcd2023c6cd013affb2eb1138d
zhangyoof <Admin123>
2020-07-28 20:47:53 +0800

    自定义基类Base

--- src/pages/base/Base.js
+++ src/pages/base/Base.js
@@ -7,18 +7,6 @@ class Base extends Component {
         debugger
         this.setState(obj)
     }
-    render(){
-        const handleClick = () => {
-            let {x,y} = this.state
-            let res = this.testFun(x,y)
-            this.setState({res })
-        }
-        return (
-            <div style={{width:'100%',height:'100%',background:'#f1f1f1'}} >
-                4564656
-            </div>
-        )
-    }
 }
 
 export default Base;
