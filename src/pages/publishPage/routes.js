22eaeb4cf17f1739d8842e271261bb6251542156
zyqatec <zhaoyaqian1993@qq.com>
2020-12-28 20:51:16 +0800

    发布会页面绘制

--- src/pages/publishPage/routes.js
+++ src/pages/publishPage/routes.js
@@ -3,6 +3,10 @@ const routes = [
     {
         path: '/publish',
         component: './publishPage/PublishPage'
+    },
+    {
+        path: '/authenticate',
+        component: './publishPage/Authentication'
     }
 ]
 module.exports = routes
