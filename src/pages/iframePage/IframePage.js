1f52738cbe6a724e09b4759138acb35f75758edd
赵雅倩 <zhaoyaqian@szewec.com>
2021-03-04 18:34:00 +0800

    ..

--- src/pages/iframePage/IframePage.js
+++ src/pages/iframePage/IframePage.js
@@ -6,7 +6,7 @@ import { router } from 'umi';
 class IframePage extends Component {
     render(){
         return (
-            <div style={{width:'100%',height:'100%',background:'#f1f1f1'}} className={styles.page1_main}>
+            <div style={{width:'100%',height:'100%'}} className={styles.page1_main}>
                 <div style={{width:500,height:500}}>
                     <iframe src = 'http://localhost:8991/page2'  style={{width:'100%',height:'100%',border:'none'}}></iframe>
                 </div>
